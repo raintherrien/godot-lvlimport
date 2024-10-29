fa7e195916676316f79c3085c6d311e95568772c
Rain Therrien <raintherrien@gmail.com>
2024-10-28 21:57:56 -0400

    More basic GDExtension setup

---
+++ src/lvlimport.hpp
@@ -0,0 +1,17 @@
+#ifndef LVLIMPORT_TEST_HPP_
+#define LVLIMPORT_TEST_HPP_
+
+#include <godot_cpp/classes/node.hpp>
+
+namespace godot {
+
+class LVLImport : public Node {
+	GDCLASS(LVLImport, Node)
+
+protected:
+	static void _bind_methods() { }
+};
+
+}
+
+#endif
---
+++ src/register_types.h
@@ -0,0 +1,11 @@
+#ifndef LVLIMPORT_REGISTER_TYPES_H
+#define LVLIMPORT_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_lvlimport_module(ModuleInitializationLevel p_level);
+void uninitialize_lvlimport_module(ModuleInitializationLevel p_level);
+
+#endif
